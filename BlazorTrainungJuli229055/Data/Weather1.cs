a981c8844a9d8a76a834e462435eb6d36b213ec8
Hannes Preishuber <hannesp@ppedv.de>
2023-07-20 11:02:23 +0200

    Dependency injection per Konstruktor in Klasse

---
+++ BlazorTrainungJuli229055/Data/Weather1.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorTrainungJuli229055.Data
+{
+    public class Weather1
+    {
+        private WeatherForecastService wfs;
+        public Weather1(WeatherForecastService _wfs)
+        {
+            wfs = _wfs;
+        }
+    }
+}
--- BlazorTrainungJuli229055/Program.cs
+++ BlazorTrainungJuli229055/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddSingleton<Weather1>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
