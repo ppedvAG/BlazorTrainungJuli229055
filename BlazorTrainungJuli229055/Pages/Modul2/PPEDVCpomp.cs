6311eae90ba6b093260ef0f1511f1028ff7defba
Hannes Preishuber <hannesp@ppedv.de>
2023-07-20 12:32:16 +0200

    Componenet Klasse mit Component Base

---
+++ BlazorTrainungJuli229055/Pages/Modul2/PPEDVCpomp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorTrainungJuli229055.Pages.Modul2
+{
+    public class PPEDVCpomp : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+
+        
+            base.BuildRenderTree(builder);
+        }
+    }
+}
