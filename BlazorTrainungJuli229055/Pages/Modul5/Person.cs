8ae510f07521671aef2cf19438d1ade35109fed0
Hannes Preishuber <hannesp@ppedv.de>
2023-07-21 11:21:48 +0200

    Person Edit Formular mit Validierung anhand von Data Annotations

---
+++ BlazorTrainungJuli229055/Pages/Modul5/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainungJuli229055.Pages.Modul5
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,ErrorMessage ="Max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(120,190,ErrorMessage ="zwischen 120 und 190")]
+        public int Gross { get; set; }
+
+    }
+}
